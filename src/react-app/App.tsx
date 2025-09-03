34b5a247cd5c32476b227d6b19fad2eee6a2b427
Ranye <drebes@simplethings.de>
2025-09-03 10:28:36 +0200

    init

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <img src="/NLZ-Logo.jpeg" alt="NLZ-BONN Logo" />
-      <h1>Willkommen im Nachwuchsleistungszentrum Bonn!</h1>
-      <h1>Hier fördern wir die Talente von morgen.</h1><br />
+      <h1>Willkommen im Taekwondo Nachwuchsleistungszentrum Bonn!</h1>
+      <h1>Hier fördern wir die Talente von morgen.</h1>
+      <br />
       <h2>Diese Webseite befindet sich noch im Aufbau und wird bald verfügbar sein.</h2>
     </div>
   );
